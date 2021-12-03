fac6a9a6976d4c55c7c7ace1b5c8b20dd950ced5
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2021-12-03 18:45:55 +0545

    Updating repo

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useState } from 'react';
+
+// css
+import './App.css';
+
+// components
+import Form from './components/Form';
+import List from './components/List';
+
+const App = () => {
+
+  const [task, setTask] = useState('');
+  const [items, setItems] = useState([]);
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  return (
+    <div className='app'>
+      <div className='header'>
+        TODOs
+      </div>
+      <Form task={task} setTask={setTask} items={items}/>
+      <List items={items} setItems={setItems} isCompleted={isCompleted} setIsCompleted={setIsCompleted} />
+    </div>
+  );
+}
+
+export default App;
